5f280089e0ce7e81135cbae1614609f9f01e68c8
Paxkgkg123 <vpels6777@gmail.com>
2023-10-18 18:27:18 +1000

    done

---
+++ task 13/Program.cs
@@ -0,0 +1,42 @@
+﻿Random rng = new Random();
+
+int number = rng.Next(0, 100);
+int result = number;
+
+while (result >= 1000) {
+    result = result / 10;
+}
+
+if (result >= 100) {
+    result %= 10;
+} else {
+    result = -1;
+}
+
+if (result != -1) {
+    Console.WriteLine($"{number} - {result}");
+} else {
+    Console.WriteLine($"{number} - 3-й цифры нет");    
+}
+
+
+//for (int i = 0; i < 30; i++) {
+//    int number = rng.Next(0, 100000);
+//    int result = number;
+
+//    while (result >= 1000) {
+//        result = result / 10;
+//    }
+
+//    if (result >= 100) {
+//        result %= 10;
+//    } else {
+//        result = -1;
+//    }
+
+//    if (result != -1) {
+//        Console.WriteLine($"{number} - {result}");
+//    } else {
+//        Console.WriteLine($"{number} - 3-й цифры нет");    
+//    }
+//}
---
+++ task 15/Program.cs
@@ -0,0 +1,14 @@
+﻿Random rng = new Random();
+
+int n = rng.Next(1, 9);
+Console.WriteLine(n);
+
+if (n < 1 || n > 7) {
+    Console.WriteLine("Такого числа нет");
+}
+if (n >= 1 && n <= 5) {
+    Console.WriteLine("Будний день");
+}
+if (n >= 6 && n <= 7) {
+    Console.WriteLine("Выходной");
+}
