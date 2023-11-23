a7943e0d860d7c9903eabbbbf60c1f1d5dd23322
Paxkgkg123 <vpels6777@gmail.com>
2023-11-23 15:30:19 +1000

    task 69

---
+++ task 69/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход 2 числа А и В, и возводит А в степень В.
+    2 3 -> 8
+ */
+ 
+int InputNumberWithMessage(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    
+    return number;
+}
+
+int Pow(int A, int B, int multy = 1)
+{
+    if (B == 0)
+        return multy;
+        
+    return Pow(A, B - 1, multy * A);
+}
+
+int A = InputNumberWithMessage("Input A: ");
+int B = InputNumberWithMessage("Input B: ");
+
+Console.Write(Pow(A, B));
