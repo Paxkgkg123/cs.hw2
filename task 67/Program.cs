163e711a2e319505172159bc6852eb7b8d7d1f51
Paxkgkg123 <vpels6777@gmail.com>
2023-11-23 15:13:29 +1000

    task 67

---
+++ task 67/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Вывести сумму цифр в числе.
+    123 -> 6
+*/
+
+int InputNumberWithMessage(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    
+    return number;
+}
+
+int SumDigits(int number, int sum = 0)
+{
+    if (number > 0)
+    {
+        sum = SumDigits(number / 10, sum + number % 10);
+    }
+    return sum;
+}
+
+int number = InputNumberWithMessage("Input number: ");
+
+Console.WriteLine(SumDigits(number));
