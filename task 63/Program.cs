7d9a9af4a3883901858b3ba9369dc56e3e5aa373
Paxkgkg123 <vpels6777@gmail.com>
2023-11-23 15:03:30 +1000

    task 63

---
+++ task 63/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Вывести все натуральные числа от 1 до N
+    5 -> 1 2 3 4 5
+*/
+
+int InputNumberWithMessage(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    
+    return number;
+}
+
+void OutputNumbers(int n)
+{
+     if (n != 1)
+     {
+        OutputNumbers(n - 1);
+     }
+     
+     Console.Write($"{n} ");
+     
+}
+
+int n = InputNumberWithMessage("Input n: ");
+
+OutputNumbers(n);
